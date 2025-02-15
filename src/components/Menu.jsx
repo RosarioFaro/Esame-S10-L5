00f866038116ac45a970d25efced21d885dd1840
RFaro <farorosario134@gmail.com>
2025-02-15 13:00:40 +0100

    upd 7.1 - messo i link alla navbar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,18 +2,23 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import icona from "../assets/meteorology.png";
+import { Link } from "react-router";
 
 function Menu() {
   return (
     <Navbar expand="lg" className="bg-warning ">
       <Container>
-        <Navbar.Brand href="#home">
-          <img src={icona} alt="icona" width={50} height={50} />
-        </Navbar.Brand>
+        <Link to="/">
+          <Navbar.Brand>
+            <img src={icona} alt="icona" width={50} height={50} />
+          </Navbar.Brand>
+        </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link as={Link} to={"/"}>
+              Home
+            </Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
           </Nav>
         </Navbar.Collapse>
